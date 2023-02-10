be2f3b558fd99315079dc482f2817edea03692d6
Olha Brylova <47885094+Morreth@users.noreply.github.com>
2023-02-10 19:35:10 +0200

    commit components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
+import { Profile } from "./Profile/Profile.js";
+import { Statistics } from "./Statistics/Statistics.js";
+import { FriendList } from "./FriendList/FriendList.js";
+
+import { TransactionHistory } from './TransactionHistory/TransactionHistory.js';
+
+import friends from './FriendList/friends.json'
+import user from './Profile/user.json';
+import data from './Statistics/data.json';
+import transactions from './TransactionHistory/transactions.json';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile userItems={user} />
+      <Statistics title="Upload stats" stats={data}></Statistics>
+      <FriendList friends={friends}></FriendList>
+      <TransactionHistory items={transactions} />
     </div>
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101'
+    //   }}
+    // >
+    //   React homework template
+    // </div>
   );
 };
